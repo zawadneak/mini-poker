fc5549d18a0e7722092032f4ca72cb4868589a32
Lucas Zawadneak <lucaszawadneak@gmail.com>
2023-05-05 16:59:31 -0300

    feat:expo router and styled components

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
-import "./styles.css";
+import styled from "styled-components/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <View>
+        <Title>Teste</Title>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const Title = styled.Text`
+  font-weight: bold;
+  margin-top: 200px;
+`;
---
+++ app/profile/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const index = (props: Props) => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  );
+};
+
+export default index;
